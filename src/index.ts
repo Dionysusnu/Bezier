07abf02cc69ebac2902ee9d49e95a80c9e2e156d
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2020-09-13 20:51:46 +0200

    Version 0.2.0 Add errors for out of bounds arguments Allow self-made classes, granted they have a compatible Lerp() method Improve example

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-type BezierCompatible = UDim2 | Vector2 | Vector3 | CFrame | Color3;
-type E = UnionToIntersection<BezierCompatible>;
-export default class BezierCurve<T extends BezierCompatible, P extends Array<T>> {
+export default class BezierCurve<T extends { Lerp(this: T, other: T, fraction: number): T }, P extends Array<T>> {
 	/**
 	 * @param points Array with input points.
 	 */
-	public constructor(private points: [...P, T]) {
-		assert(points.size() > 1);
+	public constructor(private points: [...P, P[0] extends undefined ? T : T & P[0]]) {
+		if (points.size() < 2) {
+			error("RangeError: BezierCurve input points ", 3);
+		}
 	}
 
 	/**
@@ -13,9 +13,9 @@ export default class BezierCurve<T extends BezierCompatible, P extends Array<T>>
 	 * @param n The fraction to calculate with.
 	 */
 	public calculate(n: number): T {
-		let array: Array<T> = this.getPoints(n);
+		let array: Array<T> = this.getPointsInternal(n);
 		while (array.size() > 1) {
-			array = new BezierCurve(array).getPoints(n);
+			array = new BezierCurve(array).getPointsInternal(n);
 		}
 		return array[0];
 	}
@@ -23,13 +23,20 @@ export default class BezierCurve<T extends BezierCompatible, P extends Array<T>>
 	/**
 	 * Calculates one step.
 	 * Returns an Array of length 1 smaller than the input Array.
-	 * Mostly for internal use.
 	 * @param n The fraction to calculate with.
 	 */
 	public getPoints(n: number): P {
+		// extra function call for error stack location
+		return this.getPointsInternal(n);
+	}
+
+	private getPointsInternal(n: number): P {
+		if (n < 0 || n > 1) {
+			error("RangeError: BezierCurve calculation fraction out of bounds", 3);
+		}
 		const array = [];
 		for (let i = 0; i < this.points.size() - 1; i++) {
-			array.push((this.points[i] as E).Lerp(this.points[i + 1] as E, n));
+			array.push(this.points[i].Lerp(this.points[i + 1], n));
 		}
 		return array as P;
 	}
